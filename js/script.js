554d6ba477c512617ebf4495d3ff09b9146d7c0b
Domenico159 <domenico.zummo@gmail.com>
2021-03-24 15:13:57 +0100

    Project completed beta 2.0

--- js/script.js
+++ js/script.js
@@ -6,6 +6,8 @@ cognomi.sort();
 console.log(cognomi.sort());
 var cognomeUser = prompt('Inserisci il tuo cognome').toLowerCase().trim();
 
+var items='';
+
 
 while(!isNaN(cognomeUser)){
     cognomeUser = prompt('ATTENZIONE!!! Inserire un cognome formato da caratteri alfabetici , per favore riprovare.').toLowerCase().trim();
@@ -16,6 +18,15 @@ if(isNaN(cognomeUser)){
 }
 
 
+
+for(var i = 0 ; i < cognomi.length;i++){
+
+    items += '<li>' + cognomi.sort()[i] + '<li>';
+    // console.log(items);
+}
+
+document.getElementById('cognomi').innerHTML = items;
+
 console.log(cognomi.sort());
 
-console.log('Il tuo cognome si trova alla posizione numero: ' + (cognomi.indexOf(cognomeUser)+ 1));
+document.getElementById('cognomeUser').innerHTML = 'Il tuo cognome si trova alla posizione numero: ' + (cognomi.indexOf(cognomeUser)+ 1);
